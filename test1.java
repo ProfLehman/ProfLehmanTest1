90fbd6dc4b8be10a5d3a2a243fc5c8a92b6e37da
Jeff Lehman <jlehman@huntington.edu>
2014-09-30 07:53:05 -0400

    added new comment in dev branch

--- test1.java
+++ test1.java
@@ -3,6 +3,7 @@
 //added new comment fall 2014 ... wow it has been 3.5 years since
 // I worked on this ... must have been busy :-)
 //comment for dev branch
+//adding another comment added from notepad started at github shell 
 
 public class test1 {
 
