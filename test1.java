be9416d5ce783f05f8185a362749238707726b47
Jeff Lehman <jlehman@huntington.edu>
2014-11-18 14:32:24 -0500

    added to comments

--- test1.java
+++ test1.java
@@ -2,6 +2,7 @@
 //spring 2011
 //added new comment fall 2014 ... wow it has been 3.5 years since
 // I worked on this ... must have been busy :-)
+//added another comment to test git
 
 public class test1 {
 
