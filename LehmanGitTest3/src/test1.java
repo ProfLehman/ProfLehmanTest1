f095efee2d519b8ff0342129c04e1c9ecc04c574
Jeff Lehman <jlehman@huntington.edu>
2014-11-18 15:44:25 -0500

    updated output msg

---
+++ LehmanGitTest3/src/test1.java
@@ -0,0 +1,15 @@
+//test1.java
+//spring 2011
+//added new comment fall 2014 ... wow it has been 3.5 years since
+// I worked on this ... must have been busy :-)
+//added another comment to test git
+
+public class test1 {
+
+	public static void main(String args[]) {
+
+		// changes
+		System.out.println("GIT using eclipse");
+	}
+
+}
